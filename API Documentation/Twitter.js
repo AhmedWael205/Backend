b3716e9503a4031f5d7b6d69c67ccc974d92962b
Ahmed Wael <ahmed.saeed98@eng-st.cu.edu.eg>
2019-03-08 14:31:09 +0200

    Editing the getSettings and Adding getProfileBanner and running the apidoc command in the CMD to get the html file

--- API Documentation/Twitter.js
+++ API Documentation/Twitter.js
@@ -18,9 +18,9 @@
  *
  * @apiDescription Returns settings for the authenticating user.
  *
- * @apiSuccess {boolean}   discoverable_by_email           Is the user discoverable by email.
- * @apiSuccess {string}     language    User language.
- * @apiSuccess {boolean}     protected          Is the user protected.
+ * @apiSuccess {Boolean}   discoverable_by_email           Is the user discoverable by email.
+ * @apiSuccess {String}     language    User language.
+ * @apiSuccess {Boolean}     protected          Is the user protected.
  * @apiSuccess {String} screen_name     The user's screen name.
  *
  * @apiSuccessExample {json} Success-Response:
@@ -35,3 +35,46 @@
 function getUserSettings() {
   return;
 }
+
+//---------------------------------------------------------------------------------------------------
+
+/**
+ * @api {get} /users/profile_banner The user's profile banner.
+ * @apiVersion 0.1.0
+ * @apiName GetProfileBanner
+ * @apiPermission private
+ * @apiGroup User
+ *
+ * @apiDescription Returns a map of the available size variations of the specified user's profile banner. If the user has not uploaded a profile banner, a HTTP 404 will be served instead. This method can be used instead of string manipulation on the profile_banner_url returned in user objects as described in Profile Images and Banners.
+ *  The profile banner data available at each size variant's URL is in PNG format.
+ *
+ * @apiParam {Number} user_id Optional The ID of the user for whom to return results.
+ * @apiParam {String} screen_name Optional The screen name of the user for whom to return results.
+ *
+ * @apiSuccess {Object} sizes   The Image sizes (height,width).
+ * @apiSuccess {String}  url   The Image URL.
+ *
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "sizes ": {
+ *                      "h":626,
+ *                      "w":1252
+ *                  },
+ *      "url":"https://pbs.twimg.com/profile_banners/6253282/1347394302/"
+ *     }
+ *
+ * @apiError NoBannerFound If the user has not uploaded a profile banner
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "NoBannerFound"
+ *     }
+ */
+
+function getProfileBanner() {
+  return;
+}
+
+//---------------------------------------------------------------------------------------------------
---
+++ API Documentation/doc/api_project.js
@@ -0,0 +1,19 @@
+define({
+  "name": "Twitter",
+  "version": "0.1.0",
+  "description": "Twitter Documentation",
+  "title": "Twitter",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-08T12:28:36.930Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
