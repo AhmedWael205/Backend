c6647e52edfa6729793fabf9cdaebd3109cfeb4e
ZiadHelal <48360648+ZiadHelal@users.noreply.github.com>
2019-03-10 11:59:17 +0200

    Added Favorites API

--- API Documentation/Twitter.js
+++ API Documentation/Twitter.js
@@ -1682,6 +1682,337 @@ function getUserTimeline() {
  * 
  * 
  **/
+//------------------------------------------------------------------------
+/**
+*@api {post} /Twitter/:Create Favorite(Like)
+*@apiVersion 0.1.0
+*@apiName Like
+*@apiGroup Tweets
+*
+*
+*@apiParam {Number} ID The numerical ID of the tweet to like
+*
+*@apiSuccess {Boolean} success The user did like
+*
+*
+*
+*@apiSuccessExample {json} Success-Response:
+*   HTTP/1.1 200 OK
+*   {
+*     tweet-object,
+*    "user": {user-object}
+*   }
+*/
+// ----------------------------------------------------------------------------
+/**
+*@api {post} /Twitter/:Remove Favorite(Like)
+*@apiVersion 0.1.0
+*@apiName UnLike
+*@apiGroup Tweets
+*
+*
+*@apiParam {Number} ID The numerical ID of the tweet to like
+*
+*@apiSuccess {Boolean} success The user did unlike
+*
+*
+*
+*@apiSuccessExample {json} Success-Response:
+*   HTTP/1.1 200 OK
+*   {
+*     "contributors": null,
+*     "coordinates": null,
+*     "created_at": "Wed Sep 05 00:07:01 +0000 2012",
+*     "entities": {
+*        "hashtags": [],
+*        "urls": [],
+*        "user_mentions": []
+*      },
+*      "favorited": false,
+*      "geo": null,
+*      "id": 243138128959913986,
+*      "id_str": "243138128959913986",
+*      "in_reply_to_screen_name": null,
+*      "in_reply_to_status_id": null,
+*      "in_reply_to_status_id_str": null,
+*      "in_reply_to_user_id": null,
+*      "in_reply_to_user_id_str": null,
+*      "place": null,
+*      "retweet_count": 0,
+*      "retweeted": false,
+*      "source": "Twitter for Mac",
+*      "text": "That feel when you accidentally type Bash commands into Campfire, and you also make a typo in them.",
+*      "truncated": false,
+*      "user": {
+*        "contributors_enabled": false,
+*        "created_at": "Wed Apr 23 20:32:35 +0000 2008",
+*        "default_profile": false,
+*        "default_profile_image": false,
+*        "description": "Developer at GitHub in San Francisco, CA.rnrnChicken nuggets is like my family.",
+*        "entities": {
+*          "description": {
+*            "urls": []
+*          },
+*          "url": {
+*            "urls": [
+*              {
+*                "display_url": null,
+*                "expanded_url": null,
+*                "indices": [
+*                  0,
+*                  21
+*                ],
+*                "url": "http://jakeboxer.com/"
+*              }
+*            ]
+*          }
+*        },
+*        "favourites_count": 187,
+*        "follow_request_sent": false,
+*        "followers_count": 714,
+*        "following": false,
+*        "friends_count": 327,
+*        "geo_enabled": true,
+*        "id": 14500363,
+*        "id_str": "14500363",
+*        "is_translator": false,
+*        "lang": "en",
+*        "listed_count": 39,
+*        "location": "San Francisco, CA",
+*        "name": "Jake Boxer",
+*        "notifications": false,
+*        "profile_background_color": "352726",
+*        "profile_background_image_url": "http://a0.twimg.com/images/themes/theme5/bg.gif",
+*        "profile_background_image_url_https": "https://si0.twimg.com/images/themes/theme5/bg.gif",
+*        "profile_background_tile": false,
+*        "profile_image_url": "http://a0.twimg.com/profile_images/1621757700/twitter_normal.png",
+*        "profile_image_url_https": "https://si0.twimg.com/profile_images/1621757700/twitter_normal.png",
+*        "profile_link_color": "D02B55",
+*        "profile_sidebar_border_color": "829D5E",
+*        "profile_sidebar_fill_color": "99CC33",
+*        "profile_text_color": "3E4415",
+*        "profile_use_background_image": true,
+*        "protected": false,
+*        "screen_name": "jake_boxer",
+*        "show_all_inline_media": false,
+*        "statuses_count": 5398,
+*        "time_zone": "Eastern Time (US & Canada)",
+*       "url": "http://jakeboxer.com/",
+*        "utc_offset": -18000,
+*        "verified": false
+*      }
+*   }
+*/
+// -----------------------------------------------------------------------------
+/**
+*@api {get} /Twitter/: Recent Favorites(Likes)
+*@apiVersion 0.1.0
+*@apiName LikesList
+*@apiGroup Tweets
+*
+*
+*@apiParam {Number} user_id OPTIONAL ID of user to show results.
+*@apiParam {Number} screen_name OPTIONAL The screen name of the user for whom to return results.
+*@apiParam  {Number} count OPTIONAL Specifies the number of records to retrieve.
+*
+*@apiSuccess {Boolean} success
+*
+*@apiSuccessExample {json} Success-Response:
+*     HTTP/1.1 200 OK
+*     {
+*       [
+*        {
+*          "coordinates": null,
+*          "truncated": false,
+*          "favorited": true,
+*          "created_at": "Tue Sep 04 15:55:52 +0000 2012",
+*          "id_str": "243014525132091393",
+*          "in_reply_to_user_id_str": null,
+*          "entities": {
+*            "urls": [
+*
+*            ],
+*            "hashtags": [
+*
+*            ],
+*            "user_mentions": [
+*
+*            ]
+*          },
+*          "text": "Note to self:  don't die during off-peak hours on a holiday weekend.",
+*          "contributors": null,
+*          "id": 243014525132091393,
+*          "retweet_count": 0,
+*          "in_reply_to_status_id_str": null,
+*          "geo": null,
+*          "retweeted": false,
+*          "in_reply_to_user_id": null,
+*          "in_reply_to_screen_name": null,
+*          "source": "web",
+*          "user": {
+*            "profile_sidebar_fill_color": "252429",
+*            "profile_background_tile": true,
+*            "profile_sidebar_border_color": "181A1E",
+*            "name": "Sean Cook",
+*            "profile_image_url": "http://a0.twimg.com/profile_images/1751506047/dead_sexy_normal.JPG",
+*            "location": "San Francisco",
+*            "created_at": "Sat May 09 17:58:22 +0000 2009",
+*            "follow_request_sent": false,
+*            "is_translator": false,
+*            "id_str": "38895958",
+*            "profile_link_color": "2FC2EF",
+*            "entities": {
+*              "description": {
+*                "urls": [
+*
+*                ]
+*              }
+*            },
+*            "favourites_count": 594,
+*            "url": null,
+*            "default_profile": false,
+*            "contributors_enabled": true,
+*            "profile_image_url_https": "https://si0.twimg.com/profile_images/1751506047/dead_sexy_normal.JPG",
+*            "utc_offset": -28800,
+*            "id": 38895958,
+*            "listed_count": 191,
+*            "profile_use_background_image": true,
+*            "followers_count": 10659,
+*            "protected": false,
+*            "profile_text_color": "666666",
+*            "lang": "en",
+*            "profile_background_color": "1A1B1F",
+*            "time_zone": "Pacific Time (US & Canada)",
+*            "verified": false,
+*            "profile_background_image_url_https": "https://si0.twimg.com/profile_background_images/495742332/purty_wood.png",
+*            "description": "I taught your phone that thing you like.  The Mobile Partner Engineer @Twitter. ",
+*            "geo_enabled": true,
+*            "notifications": false,
+*            "default_profile_image": false,
+*            "friends_count": 1186,
+*            "profile_background_image_url": "http://a0.twimg.com/profile_background_images/495742332/purty_wood.png",
+*            "statuses_count": 2629,
+*            "following": true,
+*            "screen_name": "theSeanCook",
+*            "show_all_inline_media": true
+*          },
+*          "place": {
+*            "name": "San Francisco",
+*            "country_code": "US",
+*            "country": "United States",
+*            "attributes": {
+*            },
+*            "url": "http://api.twitter.com/1/geo/id/5a110d312052166f.json",
+*            "id": "5a110d312052166f",
+*            "bounding_box": {
+*              "coordinates": [
+*                [
+*                  [
+*                    -122.51368188,
+*                    37.70813196
+*                  ],
+*                  [
+*                    -122.35845384,
+*                    37.70813196
+*                  ],
+*                  [
+*                    -122.35845384,
+*                    37.83245301
+*                  ],
+*                  [
+*                    -122.51368188,
+*                    37.83245301
+*                  ]
+*                ]
+*              ],
+*              "type": "Polygon"
+*            },
+*            "full_name": "San Francisco, CA",
+*            "place_type": "city"
+*          },
+*          "in_reply_to_status_id": null
+*        },
+*        {
+*          "coordinates": null,
+*          "truncated": false,
+*          "favorited": true,
+*          "created_at": "Tue Sep 04 00:17:11 +0000 2012",
+*          "id_str": "242778296117514240",
+*          "in_reply_to_user_id_str": null,
+*          "entities": {
+*            "urls": [
+*
+*            ],
+*            "hashtags": [
+*
+*            ],
+*            "user_mentions": [
+*
+*            ]
+*          },
+*          "text": "TWIT NPC. TWIT DUNGEONMASTER.",
+*          "contributors": null,
+*          "id": 242778296117514240,
+*          "retweet_count": 1,
+*          "in_reply_to_status_id_str": null,
+*          "geo": null,
+*          "retweeted": false,
+*          "in_reply_to_user_id": null,
+*          "in_reply_to_screen_name": null,
+*          "source": "Twitter for Android",
+*          "user": {
+*            "profile_sidebar_fill_color": "DDEEF6",
+*            "profile_background_tile": false,
+*            "profile_sidebar_border_color": "C0DEED",
+*            "name": "REGIS",
+*            "profile_image_url": "http://a0.twimg.com/profile_images/1812284389/allseeingeye_normal.jpg",
+*            "location": "",
+*            "created_at": "Wed May 07 19:27:16 +0000 2008",
+*            "follow_request_sent": false,
+*            "is_translator": false,
+*            "id_str": "14690653",
+*            "profile_link_color": "009999",
+*            "entities": {
+*              "description": {
+*                "urls": [
+*
+*                ]
+*              }
+*            },
+*            "favourites_count": 46407,
+*            "url": null,
+*            "default_profile": false,
+*            "contributors_enabled": false,
+*            "profile_image_url_https": "https://si0.twimg.com/profile_images/1812284389/allseeingeye_normal.jpg",
+*            "utc_offset": -25200,
+*            "id": 14690653,
+*            "listed_count": 194,
+*            "profile_use_background_image": true,
+*            "followers_count": 4198,
+*            "protected": false,
+*            "profile_text_color": "333333",
+*            "lang": "en",
+*            "profile_background_color": "131516",
+*            "time_zone": "Mountain Time (US & Canada)",
+*            "verified": false,
+*            "profile_background_image_url_https": "https://si0.twimg.com/profile_background_images/530267284/11111.JPG",
+*            "description": "Everything I tweet is or shall be.",
+*            "geo_enabled": true,
+*            "notifications": false,
+*            "default_profile_image": false,
+*            "friends_count": 200,
+*            "profile_background_image_url": "http://a0.twimg.com/profile_background_images/530267284/11111.JPG",
+*            "statuses_count": 43345,
+*            "following": true,
+*            "screen_name": "regisl",
+*            "show_all_inline_media": false
+*          },
+*          "place": null,
+*          "in_reply_to_status_id": null
+*        }
+*      ]
+*     }
+*/
 function getUserTimeline() {
   return;
 }
